1fd9ea7420377eb61ab8e113c3d9920cb1bb0f74
Eric Madar <edm@spotify.com>
2020-03-23 18:17:01 -0700

    First commit

---
+++ src/main/java/org/thedarlington/twilioquest/TwilioquestApplication.java
@@ -0,0 +1,13 @@
+package org.thedarlington.twilioquest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwilioquestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwilioquestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/thedarlington/twilioquest/TwilioquestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.thedarlington.twilioquest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwilioquestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
